7ea6e539b5153f603d95e2596c814f1de6f1e96b
Daniel Sousa <jdaniel.asousa@gmail.com>
2023-05-16 16:14:56 +0100

    feat: fix gitignore

--- packages/web/src/app/layout.tsx
+++ packages/web/src/app/layout.tsx
@@ -1,6 +1,7 @@
 import './globals.css'
 import { Inter } from 'next/font/google'
 import { ReactNode } from 'react'
+import clsx from 'clsx'
 
 const inter = Inter({ subsets: ['latin'] })
 
@@ -12,7 +13,9 @@ export const metadata = {
 export default function RootLayout({ children }: { children: ReactNode }) {
   return (
     <html lang="en">
-      <body className={inter.className}>{children}</body>
+      <body className={clsx([inter.className, 'h-screen bg-zinc-950'])}>
+        {children}
+      </body>
     </html>
   )
 }
--- packages/web/src/app/page.tsx
+++ packages/web/src/app/page.tsx
@@ -1,7 +1,9 @@
 export default function Home() {
   return (
     <main>
-      <h1>Hello World</h1>
+      <h1 className={"text-4xl font-bold text-zinc-50"}>
+        Sua cápsula do tempo
+      </h1>
     </main>
-  )
+  );
 }
