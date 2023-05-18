e25642459b478cc0e568e225283cf98e64409c0d
Daniel Sousa <jdaniel.asousa@gmail.com>
2023-05-18 14:58:02 +0100

    feat(wip): home layout

--- packages/mobile/App.tsx
+++ packages/mobile/App.tsx
@@ -1,24 +1,41 @@
-import { StatusBar } from 'expo-status-bar';
-import { Text, View } from 'react-native';
+import { StatusBar } from 'expo-status-bar'
+import { ImageBackground, Text } from 'react-native'
+import { styled } from 'nativewind'
+
+import blurBg from 'src/assets/blur.png'
+import Stripes from './src/assets/stripes.svg'
 
 import {
-  useFonts, Roboto_400Regular, Roboto_700Bold,
-} from '@expo-google-fonts/roboto';
-import { BaiJamjuree_700Bold } from '@expo-google-fonts/bai-jamjuree';
+  useFonts,
+  Roboto_400Regular,
+  Roboto_700Bold,
+} from '@expo-google-fonts/roboto'
+import { BaiJamjuree_700Bold } from '@expo-google-fonts/bai-jamjuree'
+
+const StyledStripes = styled(Stripes)
 
 export default function App() {
   const [hasLoadedFonts] = useFonts({
-    Roboto_400Regular, Roboto_700Bold, BaiJamjuree_700Bold,
-  });
+    Roboto_400Regular,
+    Roboto_700Bold,
+    BaiJamjuree_700Bold,
+  })
 
   if (!hasLoadedFonts) {
-    return null;
+    return null
   }
 
   return (
-    <View className={'flex-1 items-center justify-center bg-gray-900'}>
-      <Text className={'font-alt text-5xl font-bold text-gray-50'}>Hello Word</Text>
-      <StatusBar style='light' translucent />
-    </View>
-  );
+    <ImageBackground
+      source={blurBg}
+      className={'relative flex-1 bg-gray-900'}
+      imageStyle={{
+        position: 'absolute',
+        left: '-100%',
+      }}
+    >
+      <StyledStripes className={'absolute left-2'} />
+      <StatusBar style="light" translucent />
+    </ImageBackground>
+  )
 }
