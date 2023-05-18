d0dc292a7e7ea7eb70ebb874fe722e08188a23e1
Daniel Sousa <jdaniel.asousa@gmail.com>
2023-05-18 08:54:01 +0100

    feat: homepage

--- packages/web/src/app/page.tsx
+++ packages/web/src/app/page.tsx
@@ -1,9 +1,110 @@
+import Link from 'next/link'
+import { User } from 'lucide-react'
+import Image from 'next/image'
+
 export default function Home() {
   return (
-    <main>
-      <h1 className={"text-4xl font-bold text-zinc-50"}>
-        Sua cápsula do tempo
-      </h1>
+    <main className={'grid min-h-screen grid-cols-2'}>
+      <section
+        className={
+          'relative flex flex-col items-start justify-between overflow-hidden border-r border-white/10 bg-[url(/assets/bg-stars.svg)] bg-cover px-28 py-16'
+        }
+      >
+        {/* Blur */}
+        <div
+          className={
+            'absolute right-0 top-1/2 h-[288px] w-[526px] -translate-y-1/2 translate-x-1/2 rounded-full bg-purple-700 opacity-50 blur-full'
+          }
+        />
+
+        {/* Stripes */}
+        <div className={'absolute inset-y-0 right-2 w-2 bg-stripes'} />
+
+        {/* Sign In */}
+        <Link
+          href={'#'}
+          passHref
+          className={
+            'flex items-center gap-3 text-left transition-colors hover:text-gray-50'
+          }
+        >
+          <div
+            className={
+              'flex h-10 w-10 items-center justify-center rounded-full bg-gray-400'
+            }
+          >
+            <User className={'h-5 w-5 text-gray-500'} />
+          </div>
+
+          <p className={'max-w-[140px] text-sm leading-snug'}>
+            <span className={'underline'}>Crie sua conta</span> e salve suas
+            memórias
+          </p>
+        </Link>
+
+        {/* Hero */}
+        <div className={'space-y-5'}>
+          <Image
+            src={'/assets/nlw-spacetime.svg'}
+            alt={'NLW Spacetime'}
+            height={48}
+            width={160}
+          />
+
+          <div className={'w-full max-w-[420px] space-y-1'}>
+            <h1 className={'text-5xl font-bold leading-tight  text-gray-50'}>
+              Sua cápsula do tempo
+            </h1>
+            <p className={'text-lg leading-relaxed'}>
+              Colecione momentos marcantes da sua jornada e compartilhe (se
+              quiser) com o mundo!
+            </p>
+          </div>
+
+          <Link
+            href={'#'}
+            passHref
+            className={
+              'inline-block rounded-full bg-green-500 px-5 py-3 font-alt text-sm uppercase leading-none text-black transition-colors hover:bg-green-600'
+            }
+          >
+            Cadastrar lembrança
+          </Link>
+        </div>
+
+        {/* Footer */}
+        <footer className={'text-sm leading-relaxed text-gray-200'}>
+          Feito com 💜 no NLW da{' '}
+          <a
+            href={'https://rocketseat.com.br'}
+            className={'underline transition-colors hover:text-gray-100'}
+            target={'_blank'}
+            rel={'noreferrer'}
+          >
+            Rocketseat
+          </a>
+        </footer>
+      </section>
+
+      <section
+        className={'flex flex-col bg-[url(/assets/bg-stars.svg)] bg-cover p-16'}
+      >
+        <div className={'flex flex-1 items-center justify-center'}>
+          <p className={'w-full max-w-[360px] text-center leading-relaxed'}>
+            Você ainda não registrou nenhuma lembrança, come a{' '}
+            <Link
+              passHref
+              href={'#'}
+              className={
+                'underline transition-colors duration-150 ease-in-out hover:text-gray-50'
+              }
+            >
+              criar agora
+            </Link>
+            !
+          </p>
+        </div>
+      </section>
     </main>
-  );
+  )
 }
